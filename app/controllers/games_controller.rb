858f12aad11b67194dbd2d61dd65b41eba37f053
Daniel Castro <danielsobretudo@yahoo.com.br>
2021-08-01 16:18:44 -0300

    game structure is done

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,18 +1,57 @@
+# require "open-uri"
+
 class GamesController < ApplicationController
   def new
     vowels = %w[A E I O U Y]
     general = ('A'..'Z').to_a
     @letters = []
-    3.times { @letters << vowels.sample }
-    7.times { @letters << general.sample }
+    4.times { @letters << vowels.sample }
+    8.times { @letters << general.sample }
     @letters.shuffle!
   end
 
   def score
-     # it isn't right
-    @attempt = params[:attempt].split("")
-    @attempt.each do |letter|
-      @letters.include? letter
+    # it isn't right
+    @letters = params[:letters]
+    @attempt = params[:attempt]
+    @array = []
+    @attempt.split('').each do |letter|
+      @array << @letters.include?(letter.upcase)
     end
+    # @array.all?
+    @isword = isword?(@attempt)
   end
+
+  def isword?(word)
+    response = URI.open("https://wagon-dictionary.herokuapp.com/#{word}")
+    json = JSON.parse(response.read)
+    json['found']
+  end
+
 end
+# VOWELS = %w(A E I O U Y)
+
+  # def new
+  #   @letters = Array.new(5) { VOWELS.sample }
+  #   @letters += Array.new(5) { (('A'..'Z').to_a - VOWELS).sample }
+  #   @letters.shuffle!
+  # end
+
+  # def score
+  #   @letters = params[:letters].split
+  #   @word = (params[:word] || "").upcase
+  #   @included = included?(@word, @letters)
+  #   @english_word = english_word?(@word)
+  # end
+
+  # private
+
+  # def included?(word, letters)
+  #   word.chars.all? { |letter| word.count(letter) <= letters.count(letter) }
+  # end
+
+  # def english_word?(word)
+  #   response = URI.open("https://wagon-dictionary.herokuapp.com/#{word}")
+  #   json = JSON.parse(response.read)
+  #   json['found']
+  # end
